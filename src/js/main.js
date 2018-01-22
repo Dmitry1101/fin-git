5f9a00542637c033e7a03b77ab7b18d349349cd9
Dmitry1101 <dmitrii.dmitriev.2017@mail.ru>
2018-01-22 18:33:12 +0300

    made on Work

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
-		
+	
+	var Alll = {
+
+		$b: $('body'),
+
+		setupVars: function(){
+
+		},
+
+
+    AddEvents: function(){
+    	
+    	
+         
+    },
+
+
+    loaDed: function(){
+
+    	this.$b.addClass('loaded');
+
+    },
+
+
+    // INITIALIZED
+    init: function(){
+
+      this.setupVars();
+      this.loaDed();
+      this.AddEvents();
+
+    }
+
+  };
+  
+  Alll.init(); 
+
 });
 
 
